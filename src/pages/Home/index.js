26fff181321963f5d7e315b4db448fb7552db4ea
Shindi Toyama <shinditoyama@gmail.com>
2022-05-03 13:14:06 -0300

    test production

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,6 +4,7 @@ import Carrousel from "../../components/Carrousel";
 const Home = () => {
     return (
         <Layout>
+            Modo: {process.env.NODE_ENV}
             <Carrousel title="Filmes - Populares" type="movie" filter="popular" />
             <Carrousel title="Filmes - Próximas Estreias" type="movie" filter="upcoming" />
             <Carrousel title="Series - Populares" type="tv" filter="popular" />
